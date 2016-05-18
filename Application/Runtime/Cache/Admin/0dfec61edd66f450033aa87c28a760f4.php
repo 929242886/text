2ef56cf7d4ee0ac4a8336744abc9c1dd4b141d4b
929242886 <929242886@qq.com>
2016-05-18 19:34:14 +0800

    修改

---
+++ Application/Runtime/Cache/Admin/0dfec61edd66f450033aa87c28a760f4.php
@@ -0,0 +1,26 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>项目管理系统后台登录界面模板</title>
+<link rel="stylesheet" href="/xiangmu/text/erzu/Public/css/login.css" />
+</head>
+
+<body class="b">
+<div class="lg">
+<form action="#" method="POST">
+    <div class="lg_top"></div>
+    <div class="lg_main">
+        <div class="lg_m_1">
+        <input name="username" value="" class="ur" />
+        <input name="password" type="password" value="" class="pw" />
+        
+        </div>
+    </div>
+    <div class="lg_foot">
+    <input type="submit" value="Login In" class="bn" /></div>
+</form>
+</div>
+
+</body>
+</html>
